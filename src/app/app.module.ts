aa5b4beed541044944b6f3aebd591dd804b01ea6
Michelson Maximilian <maximilian.michelson@softronic.se>
2020-09-08 14:47:14 +0200

    angular mat 10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 
 import { NgModule } from '@angular/core';
 import { RedditTableComponent } from './app.component';
+import { AuthorNamePipe } from './author-name.pipe';
 import { AngularModule } from './modules/angular-module';
 import { AngularMaterialModule } from './modules/material-module';
-import { AuthorNamePipe } from './person-name.pipe';
 import { ReadCommentsDialogComponent } from './read-comments-dialog/read-comments-dialog.component';
 import { SelectedRowDialogComponent } from './selected-row-dialog/selected-row-dialog.component';
 
